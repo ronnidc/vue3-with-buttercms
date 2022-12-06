ef02c72adee3bad45d7230ed04539ab07f402e63
Ronni Chidekel <ronni@kudo.dk>
2022-12-06 20:10:33 +0100

    change server port and set dns to localhost

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +14,8 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+	host: 'localhost',
+	port: 3000,
+  }
 });
